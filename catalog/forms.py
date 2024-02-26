070adf27496ed9ca778fc9f40bd2ef34d345ccab
Felipe Prando <felipe.prandove@gmail.com>
2024-02-26 16:29:34 -0300

    change errors

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import *
 #Forms CMPC
 class DatosCMPCChileForm(forms.ModelForm):
-    archivo_excel = forms.FileField()
     class Meta:
         model = DatosCMPCChile
         fields = '__all__'
